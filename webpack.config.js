8ad41be9c04532331eff26b6d26d193c6847bc03
Christian Klint Labadia <51294367+thandora@users.noreply.github.com>
2023-12-18 14:26:07 +0800

    Add config for html and css plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,34 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
 module.exports = {
   entry: "./src/index.js",
+
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
+    clean: true,
   },
+
   mode: "development",
   devtool: "inline-source-map",
   devServer: {
     static: "./dist",
     port: 5500,
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
   },
 };
