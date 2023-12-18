b738c9539fa2e0de640630d116ce97c4da32d459
Christian Klint Labadia <51294367+thandora@users.noreply.github.com>
2023-12-18 15:57:13 +0800

    add homepage content

--- src/home.js
+++ src/home.js
@@ -1,4 +1,25 @@
+import restaurantImg from "./restaurant.png";
+
+const logo = document.createElement("img");
+logo.src = restaurantImg;
+
+const logoAttrib = document.createElement("a");
+logoAttrib.setAttribute("href", "https://www.flaticon.com/free-icons/restaurant");
+logoAttrib.setAttribute("title", "restaurant icons");
+logoAttrib.textContent = "Icon by Eucalyp - Flaticon";
+
+const logoContainer = document.createElement("div");
+logoContainer.classList.add("logo-container");
+logoContainer.appendChild(logo);
+logoContainer.appendChild(logoAttrib);
+
+const restaurantMessage = document.createElement("p");
+restaurantMessage.textContent =
+  "Welcome to Restaurant City's homepage! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempore quae voluptatum, odit deleniti nesciunt voluptas praesentium.";
+
 const homepage = document.createElement("div");
-homepage.textContent = "I am content";
+homepage.classList.add("homepage");
+homepage.appendChild(logoContainer);
+homepage.appendChild(restaurantMessage);
 
 export { homepage };
