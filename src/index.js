b725bb6e63d9312e3fef30d7ee7bc1721f3206b1
Christian Klint Labadia <51294367+thandora@users.noreply.github.com>
2023-12-19 15:13:27 +0800

    add navbar

--- src/index.js
+++ src/index.js
@@ -2,6 +2,34 @@ import "./style.css";
 import { homepage } from "./home";
 import { menu } from "./menu";
 
+function createNavbar() {
+  const logo = document.createElement("a");
+  logo.classList.add("logo");
+  logo.textContent = "Restaurant City";
+  logo.href = "#";
+
+  const home = document.createElement("a");
+  home.classList.add("button");
+  home.textContent = "Home";
+  home.href = "#";
+
+  const menu = document.createElement("a");
+  menu.classList.add("button");
+  menu.textContent = "Menu";
+  menu.href = "#";
+
+  const nav = document.createElement("div");
+  nav.classList.add("nav-links");
+  nav.append(home, menu);
+
+  const navbar = document.createElement("div");
+  navbar.classList.add("navbar");
+  navbar.append(logo, nav);
+
+  content.appendChild(navbar);
+}
+
 const content = document.querySelector("#content");
+createNavbar();
 content.appendChild(homepage);
 content.appendChild(menu);
